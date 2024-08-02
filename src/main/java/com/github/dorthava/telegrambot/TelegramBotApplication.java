2dc756b000605c92d1257c2e4d7e259454133f71
Dorthava <skolti96@gmail.com>
2024-08-02 20:28:07 +0300

    TB-1: Add Telegram Bot.

--- src/main/java/com/github/dorthava/telegrambot/TelegramBotApplication.java
+++ src/main/java/com/github/dorthava/telegrambot/TelegramBotApplication.java
@@ -2,8 +2,10 @@ package com.github.dorthava.telegrambot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.github.dorthava.telegrambot.bot"})
 public class TelegramBotApplication {
 
 	public static void main(String[] args) {
